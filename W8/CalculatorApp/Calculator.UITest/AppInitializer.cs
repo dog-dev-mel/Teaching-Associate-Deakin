0ed9500c2b9f0cb6ee1d5fd5279fb734d8598860
Dell_LG <lzshine@outlook.com>
2018-09-04 15:37:17 +1000

    008

---
+++ W8/CalculatorApp/Calculator.UITest/AppInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Calculator.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                //return ConfigureApp.Android.InstalledApp("com.companyname.CalculatorApp").StartApp();
+                //return ConfigureApp.Android.ApkFile("../../../CalculatorApp/bin/Debug/CalculatorApp.CalculatorApp.apk").StartApp();
+                return ConfigureApp.Android.InstalledApp("com.companyname.CalculatorApp").PreferIdeSettings().EnableLocalScreenshots().StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
