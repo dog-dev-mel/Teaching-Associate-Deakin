f74345c10189f424b5c5b87831f843cfa8e3ecad
Dell_LG <lzshine@outlook.com>
2018-09-13 12:30:29 +1000

    Final

---
+++ W9/W9OAuthLab/W9OAuthLab/W9OAuthLab.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace W9OAuthLab.Droid
+{
+    [Activity(Label = "W9OAuthLab.Droid", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+}
