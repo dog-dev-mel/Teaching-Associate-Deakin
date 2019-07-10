a9734fa14b6ee8c1e924aedc9741a8e44a31a1ec
Z LIU <Z.LIU.DEAKIN@OUTLOOK.COM>
2019-07-10 11:28:36 +1000

    Revise Week 1 lab materials

--- Week1.R
+++ Week1.R
@@ -1,90 +1,111 @@
 # Exercise 1
-#Addition
+#Addition, 5 plus 2
 5 + 2
-#Subtraction
-6 - 8
-#Multiplication
-3*4
-#Division
-3/4
-#Powers
-3^4
 
+#Subtraction, 6 minus 8
+6 - 8
+
+#Multiplication, 3 times/multiplies 4
+3*4
+
+#Division, 3 is divided by 4
+3/4
+
+#Powers, 3 to the 4 or 3 super/superscript 4
+3^4
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
 
 # Exercise 2
 #Set 5+2 to variable a
-a <- 5 + 2
+#Using less than symbol with dash assign value to the variable a 
+#To assign a negative number, -5, to the a
+a <- -5
 a
-#Set 6-8 to variable b
-b <- 6 - 8
+
+#To assign a positve number, +5, to the b
+b <- +5
 b
-#Set 3*4 to variable d
-d <- 3 * 4
-d
-#Set 3/4 to variable e
-e <- 3 / 4
-e
-#Add a to e
-a + b + d + e
+
+#Add two variables, assign the result of a plus b to the variable c
+c <- a + b
+
+#A variable can be along with a full stop symbol
+the.value <- 10
+
+#A variable can be along with a underscore/underline/underdash/low line/low dash symbol
+the.value_1 <- 5
+
+#A variable times another variable, assign its result to the variable d 
+d <- the.value * the.value_1
+
+#We also can use the euqla symbol to assign a value to a variable
+e = 1000
+
 #using function c{base} to combine Values into a Vector or List
-vector_a <- c(3,2,1,0)
+vector_a <- c(3,2,1,0) #open parenthesis 3,2,1,0 comma left parenthesis
 vector_a
+mode(vector_a)
+
 #using a set of sequential numbers to combine Values into a Vector or List
-vector_b <- 4:8
+vector_b <- 4:8  #4 colon 8
 vector_b
+mode(vector_b)
+
 #Define a empty vector using the array() command
 #The first one is the default entry of the array, e.g. 0
 #The second argument gives the length.
 vector_c <- array(0,3)
 vector_c
+mode(vector_c)
+
 vector_d <- array(1,10)
 vector_d
+mode(vector_d)
+
 vector_e <- array(2,200)
 vector_e
+mode(vector_e)
+
 #Combine the array() and c() commands 
 #for creating a complex vector includinga small sub-sequence
 sub_sequence_vector <- array(c(1,2,3,0),200)
 sub_sequence_vector
+mode(sub_sequence_vector)
 
+#You can regard the vertor as a one-dimension array
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
 
 # Exercise 3
-#Basic operations on vectors
+#Basic mathmatical operations between a vector and a normal number value
 c(1,2,3) + 3
 c(1,2,3) * 4
 c(1,2,3) ^ 2
-c(2,3) + c(5,-1)
-#vectors into a matrix using cbind (column bind) or rbind (row bind)
-cbind(c(1,2,3,7,9), c(21, 2, 1, 5, 6))
-cbind(c(1,2,3,7,9), c(1,9,7,2,1), array(6,5))
-rbind(c(3,6,1,92), c(10,3,1))
-rbind(c(3,6,1,92), c(3,2,1,8))
-rbind(c(3,6,1,92), c(4,1,12,1,2))
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+
 
 # Exercise 4
-#Assign the two vectors
-a <- c(1,6,7,9)
-b <- c(-1,2,1,-2)
-a + b
-a - b
-a * b
-a / b
-a ^ b
-
-v1 = c(1,6,7,9)
-v2 = c(-1,2,1,-2)
-v1 + v2
-v1 - v2
-v1 * v2
-v1 / v2
-v1 ^ v2
+#Basic mathmatical operations on vectors
+vector_1 <- c(1,6,7,9)
+vector_2 <- c(-1,2,1,-2)
+vector_1 + vector_2
+vector_1 - vector_2
+vector_1 * vector_2
+vector_1 / vector_2
+vector_1 ^ vector_2
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
 
 
 # Exercise 5
-#Existing functions
+#Call the R language Build-in functions
 a <- c(1,8,3,9)
 b <- c(2,2,1,1)
 d <- c(3,4,6,81,9)
-#the sum of a is 1+8+3+9
+
+#the sum of vector a is 1+8+3+9
 sum(a)
 #the product of c is 3*4*6*81*9
 prod(d)
@@ -108,10 +129,29 @@ median(a)
 mean(a)
 
 
-#Function Creating
+#Create a self-defining function
 our.mean <- function(x) {
     sum(x)/length(x)
 }
+
+our.mean(c(1,2,3,4,5))
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+nothing.happens <- function(x) {
+    sum(x) + 2
+    x = 10
+}
+nothing.happens(c(1,2,3,4))
+
+
 #we don't need to do any temporary assignment of values throughout.
 #However we could if we wanted to.
 our.mean.2 <- function(x) {
@@ -121,5 +161,11 @@ our.mean.2 <- function(x) {
     output
 }
 
-our.mean(a)
-our.mean.2(a)
+our.mean.2(c(1,2,3,4,5))
+
+#vectors into a matrix using cbind (column bind) or rbind (row bind)
+cbind(c(1,2,3,7,9), c(21, 2, 1, 5, 6))
+cbind(c(1,2,3,7,9), c(1,9,7,2,1), array(6,5))
+rbind(c(3,6,1,92), c(10,3,1))
+rbind(c(3,6,1,92), c(3,2,1,8))
+rbind(c(3,6,1,92), c(4,1,12,1,2))
